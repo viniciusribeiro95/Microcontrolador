4cc86865a5d820c31a206ee82624979b0b5aa721
viniciusribeiro95 <30874007+viniciusribeiro95@users.noreply.github.com>
2017-11-19 16:13:18 -0200

    Add files via upload

---
+++ Códigos Projeto Final/Display_BLT_Final.ino
@@ -0,0 +1,111 @@
+#if defined(__MSP430G2452__) || defined(__MSP430G2553__) || defined(__MSP430G2231__) // LaunchPad specific
+#include "Energia.h"
+#else
+#error Board not supported
+#endif
+
+// Include application, user and local libraries
+#include "LCD_5110.h"
+char c = 0;
+int i = 0;
+String s = "";
+String nome = "";
+String idade = "";
+String resp = "";
+String cont = "";
+String readString;
+
+LCD_5110 myScreen = 
+  LCD_5110(
+    P1_4,    // Chip Select 
+    P2_4,    // Serial Clock  
+    P2_1,    // Serial Data   
+    P2_0,    // Data/Command  
+    P1_5,    // Reset         
+    P2_3,    // Backlight      
+    PUSH2    // Push Button 2  
+  );
+
+
+// Add setup code 
+void setup() {   
+    myScreen.begin();
+    myScreen.setFont(0);
+    myScreen.text(0, 0, "Iniciando     Sistema");
+    myScreen.text(0, 3, "PULSEIRA      LOCALIZADORA");
+    
+    delay(3000);
+    myScreen.clear();
+    
+    Serial.begin(9600);
+
+}
+
+// Display mask supports 3 digit + one decimal place
+char display[8] = {' ', ' ', ' ', '.', ' ', 0x7f, 'C', 0x00};
+char displayCalibrationPoint[8] = {' ', ' ', ' ', '.', ' ', 0x7f, 'C', 0x00};
+
+
+// Add loop code 
+void loop() {
+  
+ while(Serial.available() && i <= 3){
+      delay(3);
+      char c = Serial.read();
+      s += c;
+      readString += c;
+      s = readString;
+ }    
+
+    if(s != ""){  
+      if(i==0){
+       nome = s;
+      }
+      if(i==1){
+        idade = s;
+      }
+      if(i==2){
+        resp = s;
+      }
+      if(i==3){
+        cont = s;
+      }
+     readString = "";
+     s = "";
+     i++;
+ }
+     
+  
+  myScreen.clear();
+  myScreen.text(0,0,"Nome:");
+  myScreen.text(0,1,nome);
+  myScreen.text(0,3,"Idade: ");
+  myScreen.text(0,4,idade);
+  delay (2000);
+  
+  myScreen.clear();
+  myScreen.text(0,0,"Responsavel:");
+  myScreen.text(0,1,resp);
+  myScreen.text(0,3,"Contato: ");
+  myScreen.text(0,4,cont);
+  delay (2000);
+//}
+//else{
+// 
+//  myScreen.clear();
+//  myScreen.text(0,0,"Nome:");
+//  myScreen.text(0,1,s);
+//  myScreen.text(0,3,"Idade: ");
+//  myScreen.text(0,4,"21 Anos");
+//  delay (2000);
+//  
+//  myScreen.clear();
+//  myScreen.text(0,0,"Responsavel:");
+//  myScreen.text(0,1,"Eu mesmo");
+//  myScreen.text(0,3,"Contato: ");
+//  myScreen.text(0,4,"(61)99884-1011");
+//  delay (2000);
+//}
+}
+
+
---
+++ Códigos Projeto Final/GPS_MSP_ENERGIA.ino
@@ -0,0 +1,65 @@
+#include <TinyGPS++.h>
+//#include <SoftwareSerial.h>
+/* Create an object named gps of the class TinyGPSPlus */
+TinyGPSPlus gps;  
+//SoftwareSerial ss(3,4);
+
+//
+//volatile float minutes, seconds;
+//volatile int degree, secs, mins;
+
+char server[] = "https://maps.google.com.br/maps?q=";
+String data;
+
+float dado_lat; // variáveis de armazenamento
+char lat_aux[4]; // variáveis de armazenamento
+String latParaString; // variáveis de armazenamento
+float dado_lng; // variáveis de armazenamento
+char lng_aux[4]; // variáveis de armazenamento
+String lngParaString; // variáveis de armazenamento
+
+
+void setup() {
+  Serial.begin(4800);/* Define baud rate for serial communication */-
+ //ss.begin(9600);
+  
+
+}
+
+void loop() {
+       smartDelay(1000); /* Generate precise delay of 1ms */
+  if (gps.location.isValid())
+        {          
+    dado_lat=gps.location.lat();
+    dado_lng=gps.location.lng();
+   
+    dtostrf(dado_lat,4,4,lat_aux);
+    latParaString = String(lat_aux);
+   
+    dtostrf(dado_lng,4,4,lng_aux);
+    lngParaString = String(lng_aux);
+   
+    data+=(server);
+    data+=(latParaString);
+    data+=",";
+    data+=(lngParaString);
+    Serial.println(data);
+   
+  }
+  else{
+    Serial.println("S");
+  }
+
+}
+
+static void smartDelay(unsigned long ms)
+{
+  unsigned long start = millis();
+  do 
+  {
+    while (Serial.available())  /* Encode data read from GPS while data is available on serial port */
+      gps.encode(Serial.read());
+    /* Encode basically is used to parse the string received by the GPS and to store it in a buffer so that information can be extracted from it */
+  } while (millis() - start < ms);
+}
+
